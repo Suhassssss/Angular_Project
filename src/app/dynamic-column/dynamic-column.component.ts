d3a3fe71f66818c5621e4ffdbcd0bf6371b68e81
Suhassssss <104508217+Suhassssss@users.noreply.github.com>
2023-04-18 16:35:09 +0530

    new component created

---
+++ src/app/dynamic-column/dynamic-column.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-column',
+  templateUrl: './dynamic-column.component.html',
+  styleUrls: ['./dynamic-column.component.css']
+})
+export class DynamicColumnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
