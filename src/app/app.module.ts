3e763c0963e6ac98f18abe5a4f97b177e8c958e1
Suhassssss <104508217+Suhassssss@users.noreply.github.com>
2023-04-18 16:34:35 +0530

    bootstrap installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DynamicColumnComponent } from './dynamic-column/dynamic-column.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DynamicColumnComponent
   ],
   imports: [
     BrowserModule,
