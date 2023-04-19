03c54989dfc5fd986e01903dc484b2b1317776db
Suhassssss <104508217+Suhassssss@users.noreply.github.com>
2023-04-19 08:30:48 +0530

    style and some functionality changes

--- src/app/dynamic-column/dynamic-column.component.ts
+++ src/app/dynamic-column/dynamic-column.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import * as sampleData from '../../assets/sample.json';
+import * as sampleData from 'src/assets/sample.json';
 @Component({
   selector: 'app-dynamic-column',
   templateUrl: './dynamic-column.component.html',
@@ -15,20 +15,51 @@ export class DynamicColumnComponent implements OnInit {
   search:string="";
   collectionSize = this.rowData.length;
   data = this.rowData;
+  checked:boolean = false;
+  count: number = 0;
+  selectedMsg: string = "0 row selected";
+  allChecked: boolean = false;
   constructor() { 
-    console.log(this.columnData, this.rowData);
 this.refreshData();
     
   }
   refreshData() {
     this.data = this.rowData
-      .map((data, i) => ({id: i + 1, ...data}))
       .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
+    
   }
   reverse: boolean = false;
   sort(){
     this.reverse = !this.reverse;
-    
+  }
+  selectcount(arg){
+    console.log(arg.target.checked);
+    if (arg.target.checked == true) {
+      this.count++;
+    }
+    else this.count--;
+    this.selectedMsg = this.count == 1? `1 row selected`: `${this.count} rows selected`;
+    console.log(this.selectedMsg);
+    if(this.count < this.pageSize){
+      this.allChecked = false;
+    }
+    if(this.count === this.pageSize){
+      this.allChecked = true;
+    }
+      }
+  selectAll(arg){
+    this.checked = true;
+    this.allChecked = !this.allChecked;
+    if (arg.target.checked == false) {
+      this.count=0;
+      this.checked = false;
+    }
+    else{
+      this.checked = true;
+      this.count = this.pageSize;
+    }
+    console.log(arg.target.checked);
+    this.selectedMsg = `${this.count} rows selected`;
   }
   ngOnInit(): void {
   }
